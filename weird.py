a15ba7825b3826013bf07b1c8ea4154d6fc01e93
Naod <124173404+naod-g@users.noreply.github.com>
2026-02-03 19:25:39 +0300

    Implement weird number classification logic

---
+++ weird.py
@@ -0,0 +1,21 @@
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+    
+    
+if n %2 == 0:
+    if n in range(2, 5+1):
+        print("Not Weird")
+    elif n in range(6, 20+1):
+        print("Weird")
+    else:
+        print("Not Weird")
+else:
+    print("Weird")
