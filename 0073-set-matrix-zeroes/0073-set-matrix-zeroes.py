b456e31227132fabcf4d49f334a744d89e0605b3
Naod <124173404+naod-g@users.noreply.github.com>
2026-02-12 14:30:37 +0300

    Time: 3 ms (69.59%), Space: 20.4 MB (13.54%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -1,8 +1,5 @@
 class Solution:
     def setZeroes(self, matrix: List[List[int]]) -> None:
-        """
-        Do not return anything, modify matrix in-place instead.
-        """
         row_count = len(matrix)
         col_count = len(matrix[0])
 
