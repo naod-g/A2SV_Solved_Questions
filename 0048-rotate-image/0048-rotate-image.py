75b4f6e55f7c2cc55169db8792d613877de50f34
Naod <124173404+naod-g@users.noreply.github.com>
2026-02-12 14:52:30 +0300

    Time: 0 ms (100%), Space: 19.4 MB (20.39%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.py
+++ 0048-rotate-image/0048-rotate-image.py
@@ -1,6 +1,7 @@
 class Solution:
     def rotate(self, matrix: List[List[int]]) -> None:
-        # this is transposed but in tuple format "list(zip(*matrix))"
-        res = [list(row)[::-1] for row in list(zip(*matrix))]
-        matrix[:] = res
+        matrix[:] = [list(row)[::-1] for row in list(zip(*matrix))]
+
+        # list(zip(*matrix)) = Transpose 
+        # list(row)[::-1] = reverse the transposed and chnaged it to list instead of tuple
         
