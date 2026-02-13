cf1b013110d447bc6e942beaceb73d58064486f8
Naod <124173404+naod-g@users.noreply.github.com>
2026-02-13 11:00:51 +0300

    Time: 143 ms (62.17%), Space: 20.1 MB (58.43%) - LeetHub

---
+++ 0661-image-smoother/0661-image-smoother.py
@@ -0,0 +1,24 @@
+class Solution:
+    def imageSmoother(self, img: List[List[int]]) -> List[List[int]]:
+        row_count = len(img)
+        col_count = len(img[0])
+        res = [[0] * col_count for i in range(row_count)]
+
+        directions = [  (-1,-1), (-1,0), (-1,1),
+                        (0, -1), (0, 0), (0, 1),
+                        (1, -1), (1, 0), (1, 1) ]
+
+        for row in range(row_count):
+            for col in range(col_count):
+                total = 0
+                count = 0
+                for dr, dc in directions: 
+                    nr, nc = row + dr, col + dc
+
+                    if 0 <= nr < row_count and 0 <= nc < col_count:
+                        total += img[nr][nc]
+                        count += 1
+                res[row][col] = total // count
+                
+
+        return res
