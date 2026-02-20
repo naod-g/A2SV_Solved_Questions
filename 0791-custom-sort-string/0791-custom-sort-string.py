ac79ae589f5466857ac61834d198e1c1dd400a90
Naod <124173404+naod-g@users.noreply.github.com>
2026-02-20 18:33:22 +0300

    Comment out old implementation of customSortString
    Comment out the previous implementation of customSortString.

--- 0791-custom-sort-string/0791-custom-sort-string.py
+++ 0791-custom-sort-string/0791-custom-sort-string.py
@@ -2,3 +2,16 @@ class Solution:
     def customSortString(self, order: str, s: str) -> str:
         return "".join(sorted(s, key = lambda ch: order.index(ch) if ch in order else len(order) ))
 
+        # res = []
+        # count = Counter(s)
+
+        # for ch in order:
+        #     if ch in s:
+        #         res.append(ch * count[ch])
+
+        # res = "".join(res)
+        # for ch in s:
+        #     if ch not in res:
+        #         res += (ch * count[ch])
+
+        # return res
