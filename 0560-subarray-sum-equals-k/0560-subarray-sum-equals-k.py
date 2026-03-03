7aba20742029d207162e796a8d0430c024f04b6d
Naod <124173404+naod-g@users.noreply.github.com>
2026-03-03 09:35:25 +0300

    Time: 67 ms (6.31%), Space: 22 MB (54.93%) - LeetHub

---
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -0,0 +1,16 @@
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        count = 0
+        pre = [nums[0]] * len(nums)
+
+        for i in range(1, len(nums)):
+            pre[i] = pre[i-1] + nums[i]
+
+        freq = Counter()
+        for num in pre:
+            if num == k:
+                count += 1
+            count += freq[num-k]    
+            freq[num] += 1
+            
+        return count
