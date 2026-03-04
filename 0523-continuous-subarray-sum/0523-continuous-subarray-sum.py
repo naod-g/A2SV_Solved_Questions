baffcc774f52591efe11931d7feef0dfbbb6dc96
Naod <124173404+naod-g@users.noreply.github.com>
2026-03-04 17:40:12 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0523-continuous-subarray-sum/0523-continuous-subarray-sum.py
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.py
@@ -1,33 +1,15 @@
 class Solution:
     def checkSubarraySum(self, nums: List[int], k: int) -> bool:
         n = len(nums)
-        pre = [nums[0]] * n
-        moded = []
-        mp = defaultdict(list)
-
-        if len(nums) < 2:
-            return False
-
-        for i in range(1, n):
-            pre[i] = (pre[i-1] + nums[i])
-
+        pref = {0:-1}
+        summ = 0
+        
         for i in range(n):
-            moded.append(pre[i] % k)
-
-        for i, v in enumerate(moded):
-            mp[v].append(i)
-
-        for k, indexes in mp.items():
-            if len(indexes) > 1:
-                if indexes[-1] - indexes[0] >= 2:
+            summ+=nums[i]
+            if summ%k in pref:
+                if i-pref[summ%k] >= 2:
                     return True
-
-        for i in range(n):
-            if moded[i] == 0 and i > 0:
-                if pre[i] != 0:
-                    return True
-        for i in range(1, n):
-            if moded[i] == 0:
-                return True
-
+            else:
+                pref[summ%k] = i
+        
         return False
