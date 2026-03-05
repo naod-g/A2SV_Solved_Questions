3decba8268deee814f39b25bec36d8785e691754
Naod <124173404+naod-g@users.noreply.github.com>
2026-03-05 13:39:18 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2381-shifting-letters-ii/2381-shifting-letters-ii.py
+++ 2381-shifting-letters-ii/2381-shifting-letters-ii.py
@@ -1,26 +1,19 @@
 class Solution:
     def shiftingLetters(self, s: str, shifts: List[List[int]]) -> str:
-        pre = [0] * len(s)
+        n = len(s)
+        pre = [0] * n
         mp = {}
         for i in range(0, 26):
             mp[i] = chr(i+97)
         
         for l, r, d in shifts:
             pre[l] += 1 if d == 1 else -1
-            if r + 1 < len(s):
+            if r + 1 < n:
                 pre[r+1] -= 1 if d == 1 else -1
 
-        for i in range(1, len(s)):
+        for i in range(1, n):
             pre[i] += pre[i-1]
         
-        res = []
-        for i in range(len(s)):
-            curr = (ord(s[i]) + pre[i]) - 97
-            curr %= 26
-            res.append(mp[curr])
+        res = [mp[((ord(s[i]) + pre[i]) - 97) % 26] for i in range(n)]
 
-            
-
-        print(pre)
-        print(res)
         return "".join(res)
