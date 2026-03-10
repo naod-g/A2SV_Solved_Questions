9dfda1a98ac6ea007e274163a1fab3af1dc45288
Naod <124173404+naod-g@users.noreply.github.com>
2026-03-10 18:52:53 +0300

    Time: 0 ms (100%), Space: 19.4 MB (89.92%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.py
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -1,12 +1,12 @@
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        mp = defaultdict(lambda: -1)
         stack = []
+        mp = defaultdict(lambda : -1)
 
         for num in nums2:
             while stack and stack[-1] < num:
                 mp[stack.pop()] = num
-                
+
             stack.append(num)
 
         return [mp[num] for num in nums1]
