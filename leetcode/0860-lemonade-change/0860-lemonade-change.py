74a2dfc3b7d1d867a85eb165e9e36ada40d984be
Naod <124173404+naod-g@users.noreply.github.com>
2026-03-13 18:43:04 +0300

    Add solution for Lemonade Change

---
+++ leetcode/0860-lemonade-change/0860-lemonade-change.py
@@ -0,0 +1,24 @@
+class Solution:
+    def lemonadeChange(self, bills: List[int]) -> bool:
+        have = {5:0, 10:0}
+
+        for bill in bills:
+            if bill == 5:
+                have[5] += 1
+
+            elif bill == 10:
+                if have[5] < 1:
+                    return False
+                else:
+                    have[5] -= 1
+                    have[10] += 1
+            else:
+                if have[10] > 0 and have[5] > 0:
+                    have[5] -= 1
+                    have[10] -= 1
+                    continue
+                elif have[5] > 2:
+                    have[5] -= 3
+                else:
+                    return False
+        return True
