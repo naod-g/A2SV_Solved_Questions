b5abc4695b8404d1f890068aa6ed04752028dbe8
Naod <124173404+naod-g@users.noreply.github.com>
2026-03-17 17:26:43 +0300

    Time: 0 ms (100%), Space: 19.4 MB (95.04%) - LeetHub

--- 0050-powx-n/0050-powx-n.py
+++ 0050-powx-n/0050-powx-n.py
@@ -1,17 +1,10 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
-        memo = {}
-        def help(num, p):
-            if p == 1:
-                return num
-            if p in memo:
-                return memo[p]
-            left, right = p // 2, ceil(p / 2)
-            memo[p] = help(num, left) * help(num, right)
-            return memo[p]
-        if n < 0:
-            ans = help(x, -n)
-            return 1 / ans
         if n == 0:
-            return 1
-        return help(x, n)
+            return 1.0
+        if n == 1:
+            return x
+        if n < 0:
+            return 1 / self.myPow(x, -n)
+        tmp = self.myPow(x, n // 2)
+        return tmp *  tmp * (x if n % 2 else 1)
