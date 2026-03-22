617baad7fe8e41e6935af0fd5ead16d6ed3aaf9a
Naod <124173404+naod-g@users.noreply.github.com>
2026-03-22 14:38:36 +0300

    Time: 8 ms (49.73%), Space: 20.7 MB (41.8%) - LeetHub

---
+++ 0324-wiggle-sort-ii/0324-wiggle-sort-ii.py
@@ -0,0 +1,8 @@
+class Solution:
+    def wiggleSort(self, nums: List[int]) -> None:
+        nums.sort()
+        mid = (len(nums) - 1) // 2
+        nums[::2], nums[1::2] = nums[mid::-1], nums[:mid:-1]
+
+
+        
