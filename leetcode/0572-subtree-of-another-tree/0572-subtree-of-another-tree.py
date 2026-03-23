0abf08b7c6ddd15e381af6fa8755093fa8b82971
Naod <124173404+naod-g@users.noreply.github.com>
2026-03-23 18:13:26 +0300

    Add solution for Subtree of Another Tree

---
+++ leetcode/0572-subtree-of-another-tree/0572-subtree-of-another-tree.py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+        def compare(s, t):
+            if not(s and t):
+                return s is t
+
+            if s.val == t.val:
+                if compare(s.left, t.left) and compare(s.right, t.right):
+                    return True
+            return False
+        
+        if compare(root, subRoot):
+            return True
+        if not root: 
+            return False
+        return (self.isSubtree(root.right, subRoot) or self.isSubtree(root.left, subRoot))
