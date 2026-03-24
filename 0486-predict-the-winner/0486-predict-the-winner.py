d6a6d56335347511762c1141de089c49e8628f45
Naod <124173404+naod-g@users.noreply.github.com>
2026-03-24 16:30:46 +0300

    Time: 1751 ms (11.89%), Space: 18.8 MB (99.83%) - LeetHub

---
+++ 0486-predict-the-winner/0486-predict-the-winner.py
@@ -0,0 +1,12 @@
+class Solution:
+    def predictTheWinner(self, nums: List[int]) -> bool:
+        def func(i, j, turn):
+
+            if i == j:
+                return nums[i]
+            if turn:
+                return max(nums[i] + func(i+1,j, not turn), nums[j] + func(i,j-1, not turn))
+            return min(-nums[i] + func(i+1,j, not turn), -nums[j] + func(i,j-1, not turn))
+
+        return func(0, len(nums)-1, True) >= 0
+            
