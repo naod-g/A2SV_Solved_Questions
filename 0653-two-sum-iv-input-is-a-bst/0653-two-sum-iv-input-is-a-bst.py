8dac808a93bcb65a0377a2a3ae4b5498ae99902c
Naod <124173404+naod-g@users.noreply.github.com>
2026-03-27 19:51:09 +0300

    Time: 8 ms (32.57%), Space: 22 MB (85.04%) - LeetHub

---
+++ 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.py
@@ -0,0 +1,34 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def findTarget(self, root: Optional[TreeNode], k: int) -> bool:
+        nums = []
+        def dfs(node):
+            if not node: return
+            if node.left: dfs(node.left)
+            if node.right: dfs(node.right)
+            nums.append(node.val)
+        dfs(root)
+
+        i = 0
+        j = len(nums) - 1
+        nums.sort()
+        
+        while i < j:
+            if nums[i] + nums[j] == k:
+                return True
+                break
+            elif nums[i] + nums[j] > k:
+                j -= 1
+            else:
+                i += 1
+
+        return False
+
+
+
+            
