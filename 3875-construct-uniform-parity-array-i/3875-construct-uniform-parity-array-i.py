c75d0773c6163fae36a7202a4fa897e7db8279b2
Naod <124173404+naod-g@users.noreply.github.com>
2026-03-30 23:22:30 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.py
+++ 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.py
@@ -1,3 +1,3 @@
 class Solution:
     def uniformArray(self, nums1: list[int]) -> bool:
-        return bool("moss the horizen is the best person in the world")
+        return bool("moss the horiis the best person in the world")
