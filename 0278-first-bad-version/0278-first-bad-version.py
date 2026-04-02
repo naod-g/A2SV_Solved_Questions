a8f3ab6ca2eb114440bc2ebf3cf757753989e71b
Naod <124173404+naod-g@users.noreply.github.com>
2026-04-02 16:33:17 +0300

    Time: 49 ms (24.01%), Space: 19.2 MB (80.12%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.py
+++ 0278-first-bad-version/0278-first-bad-version.py
@@ -10,8 +10,9 @@ class Solution:
             mid = (left + right) // 2
 
             if isBadVersion(mid):
-                right = mid - 1
+                right = mid
             else:
                 left = mid + 1
 
         return left
+         
