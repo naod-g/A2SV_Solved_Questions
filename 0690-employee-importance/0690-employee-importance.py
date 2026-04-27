98d92d489ff32968a8ae007312ede38d84581f9e
Naod <124173404+naod-g@users.noreply.github.com>
2026-04-27 10:34:04 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0690-employee-importance/0690-employee-importance.py
+++ 0690-employee-importance/0690-employee-importance.py
@@ -19,6 +19,7 @@ class Solution:
 
                     for sub_id in e.subordinates:
                         dfs(sub_id)
+                    break
                     
         dfs(id)
         return res
